0512ea1982ff39eb1097623e8bf8e2a4afeaaafc
Antoine Lauzis <antoine.lauzis@etudiant.univ-reims.fr>
2024-03-10 17:41:57 +0100

    feat : add variables nd improve style

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': 'rgba(137, 35, 57, 1)',
+        'dark': 'rgba(83, 27, 39, 1)',
+      }
+    },
+    fontFamily: {
+      'lexend': ["Lexend", "sans-serif"], 
+    },
   },
   plugins: [],
 }
